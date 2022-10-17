e03e9a276085cb630adf958ffa6304d1ae4eaa92
ben fitzpatrick <dfitz@me.com>
2022-10-16 23:28:51 -0400

    uploaded personal build files
    Signed-off-by: ben fitzpatrick <dfitz@me.com>

---
+++ install-stormos-kde/build.sh
@@ -0,0 +1,2 @@
+ÝLJåQ´l7–rÊj‹Æ§ÖÉÔ«’;}l|þ/Ûª_æK¢j¯×‹ymûÂdÛÂ›LQ·E[eÏÔwn`|–<`[²0ê²ñï6èªæŽãLQv[¡Ðéä‘q\{)ê²°û‹Õ<S8Þ%«lŠºgŠžÇ¢®j$wÊªqŠEíË-«¨+)X§šÊºjÉøq›
+}$\îž»’Bž÷·£Ò9ÉÒiaáH'T:,ÜÓ¸Üñ0eX¿­¡Éž”b[C×dÙ´u·p“ïjú`ÖdS—·ûp÷˜Ôº¢Ú"sQW:«ÒôýLßÏôýLß‡(.j&¦§^ù¹ãáþpÿÉ©‰i
---
+++ install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+#
+##############################################################################
+#
+#  PostInstall is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your discretion) any later version.
+#
+#  PostInstall is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+##############################################################################
+
+# clean out archiso files from install
+rm -f /etc/sudoers.d/g_wheel
+rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
+rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
+rm /root/{.automated_script.sh,.zlogin}
+rm /etc/mkinitcpio-archiso.conf
+rm -r /etc/initcpio
+
+#remove calamares and abif residue
+rm -rf /usr/share/calamares
+rm -rf $HOME/.config/autostart/calamares.desktop
+rm -rf /usr/share/applications/abif.desktop
+
+echo "FONT=ter-p16n" >> /etc/vconsole.conf
+
+# Continue cleanup
+rm /usr/local/bin/postinstall.sh
