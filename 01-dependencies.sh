c0edaae631b35c0b61aef9eeb2a6112538c31b40
senalbulumulle <senal.bulummulle@gmail.com>
2023-06-18 16:18:12 +0000

    Added virt-manager

--- 01-dependencies.sh
+++ 01-dependencies.sh
@@ -3,5 +3,6 @@
 sudo pacman -S archiso --noconfirm
 sudo pacman -S qemu-full --noconfirm
 sudo pacman -S libvirt --noconfirm
+sudo pacman -S virt-manager --noconfirm
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
