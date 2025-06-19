b5ea03d922477cc35fae213bb07f011d2a8c5cb2
bfitzgit23 <dfitz@me.com>
2025-06-19 21:03:01 +0000

    Edit profiledef.sh

--- install-stormos-xfce/profiledef.sh
+++ install-stormos-xfce/profiledef.sh
@@ -14,8 +14,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M' )
 file_permissions=(
   ["/root"]="0:0:750"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
