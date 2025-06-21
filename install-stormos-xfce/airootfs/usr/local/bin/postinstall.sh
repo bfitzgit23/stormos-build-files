ab3aeb01d511b13030037b552dbbdffed727654e
ben <dfitz@me.com>
2025-06-21 13:31:29 -0400

    Update postinstall.sh

--- install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
+++ install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
@@ -19,7 +19,6 @@ rm -f /etc/sudoers.d/g_wheel
 rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
 rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
-rm /etc/mkinitcpio-archiso.conf
 rm -r /etc/initcpio
 
 echo "FONT=ter-p16n" >> /etc/vconsole.conf
