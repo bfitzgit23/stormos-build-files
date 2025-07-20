2cfb4f639200ec771f6c87868e21c854f1149048
bfitzgit23 <dfitz@me.com>
2025-07-20 18:26:30 -0400

    attempting fix to desktop themes

--- install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
+++ install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
@@ -17,7 +17,6 @@
 # clean out archiso files from install
 rm -f /etc/sudoers.d/g_wheel
 rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
-rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 
 echo "FONT=ter-p16n" >> /etc/vconsole.conf
