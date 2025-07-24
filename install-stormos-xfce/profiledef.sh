7f3a24570d66ef566c18c1a1fd7163636def7b29
ben <dfitz@me.com>
2025-07-23 22:22:09 -0400

    Update profiledef.sh

--- install-stormos-xfce/profiledef.sh
+++ install-stormos-xfce/profiledef.sh
@@ -17,6 +17,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M' )
 file_permissions=(
   ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
