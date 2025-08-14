31e43515e6e5d125d1c5826a2ffb47a8c880ba4b
ben <dfitz@me.com>
2025-08-14 01:21:37 -0400

    Update postinstall.sh

--- install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
+++ install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
@@ -1,56 +1,46 @@
 #!/bin/bash -e
 #
 ##############################################################################
-#
-#  PostInstall is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your discretion) any later version.
-#
-#  PostInstall is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
+#  PostInstall - StormOS setup script
+#  Licensed under GPLv3 or later
 ##############################################################################
 
-# Better way to get the username - assuming the script is run by the target user
-name=$(whoami)
-REAL_NAME=/home/$USER
+# Get the actual logged-in user, even if script runs as root
+USER_NAME=$(logname)
 
-# genfstab -U / > /etc/fstab/
+# Remove unwanted launchers
+rm -f "/home/$USER_NAME/Desktop/calamares.desktop" || true
+rm -f "/home/$USER_NAME/Desktop/abif.desktop" || true
 
-mkdir -p /home/$USER/.config
-mkdir -p /home/$USER/.local
-mkdir -p /home/$USER/Desktop
-mkdir -p /home/$USER/Music
-mkdir -p /home/$USER/.oh-my-bash
+# Trust all remaining .desktop files on Desktop (run as real user)
+if [ -d "/home/$USER_NAME/Desktop" ]; then
+    sudo -u "$USER_NAME" bash -c '
+        find "$HOME/Desktop" -type f -name "*.desktop" \
+            -exec chmod +x {} \; \
+            -exec gio set {} "metadata::trusted" true \;
+    '
+fi
 
-#cp -r /cinnamon-configs/cinnamon-stuff/nemo/* /home/$USER/.config/nemo
-cp -r /usr/share/oh-my-bash/* /home/$USER/.oh-my-bash/
-mkdir -p /home/$USER/.config/autostart
+# Create required directories
+mkdir -p /home/$USER_NAME/.config
+mkdir -p /home/$USER_NAME/.local
+mkdir -p /home/$USER_NAME/Desktop
+mkdir -p /home/$USER_NAME/Music
+mkdir -p /home/$USER_NAME/.oh-my-bash
+mkdir -p /home/$USER_NAME/.config/autostart
 
-#cp -r /root/stormos.png /home/$USER/stormos.png
+# Copy configurations and themes
+cp -r /usr/share/oh-my-bash/* /home/$USER_NAME/.oh-my-bash/ || true
+cp -r /etc/skel/.config/* /home/$USER_NAME/.config/ || true
+cp -r /etc/skel/.local/* /home/$USER_NAME/.local || true
 
-# create python fix!
-cp -r /etc/skel/.config/* /home/$USER/.config/
-cp -r /etc/skel/.local/* /home/$USER/.local 
-cp -r /usr/share/oh-my-bash/* /home/$USER/.oh-my-bash/
-
-#mkdir -p /usr/lib/python3.13/site-packages/six
-#touch /usr/lib/python3.13/site-packages/six/__init__.py
-#cp /usr/lib/python3.12/site-packages/six.py /usr/lib/python3.13/site-packages/six/six.py
-
-# mkdir /home/$USER/.local/share/cinnamon
-# cp -r /cinnamon-configs/cinnamon-stuff/extensions /home/$USER/.local/share/cinnamon/
-
+# Set Plymouth theme and sudo feedback
 plymouth-set-default-theme stormos
 echo "Defaults pwfeedback" | sudo EDITOR='tee -a' visudo >/dev/null 2>&1
 
-USERNAME=$(whoami)
+# Prepare XFCE backgrounds
 mkdir -p /usr/share/backgrounds/xfce
 cp /usr/share/backgrounds/* /usr/share/backgrounds/xfce/ || true
 
-# Changed from rm to mv to /dev/null
-rm -f "/home/$USER/Desktop/calamares.desktop"
-rm -f "/home/$USER/Desktop/abif.desktop"
+# Fix ownership of all files we touched
+chown -R "$USER_NAME:$USER_NAME" /home/$USER_NAME
