de12275933eb13610cd4fc0ee5792e0f906a8699
ben <dfitz@me.com>
2025-08-14 20:18:18 -0400

    Update postinstall.sh

--- install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
+++ install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
@@ -39,10 +39,9 @@ plymouth-set-default-theme stormos
 echo "Defaults pwfeedback" | sudo EDITOR='tee -a' visudo >/dev/null 2>&1
 
 # Prepare XFCE backgrounds
-rm -rf /usr/share/backgrounds/xfce
 
 mkdir -p /usr/share/backgrounds/xfce
-cp /usr/share/backgrounds/*.* /usr/share/backgrounds/xfce/ || true
+cp /usr/share/backgrounds/* /usr/share/backgrounds/xfce/ || true
 
 find "/home/$USER_NAME/Desktop" -name ".desktop" -exec chmod +x {} ;
 find "/home/$USER_NAME/Desktop" -name ".desktop" -exec gio set {} "metadata::trusted" yes ;
