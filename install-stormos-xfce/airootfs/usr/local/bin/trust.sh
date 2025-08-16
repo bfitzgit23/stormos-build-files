1cbde1f01f93a0deb981c9520ebd24a154cb7111
ben <dfitz@me.com>
2025-08-16 13:39:00 -0400

    Update trust.sh

--- install-stormos-xfce/airootfs/usr/local/bin/trust.sh
+++ install-stormos-xfce/airootfs/usr/local/bin/trust.sh
@@ -1,1 +1,1 @@
-f=FILE; gio set -t string $f metadata::xfce-exe-checksum "$(sha256sum $f | awk '{print $1}')"
+for f in ~/Desktop/*.desktop; do chmod +x "$f"; gio set -t string "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"; done
