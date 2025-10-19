b2db2d488067f29c1d2bcc7b7efe0737afed0817
ben <dfitz@me.com>
2025-10-19 15:20:45 -0400

    Update postinstall.sh

--- install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
+++ install-stormos-xfce/airootfs/usr/local/bin/postinstall.sh
@@ -140,3 +140,6 @@ fi
 echo "DNS: Configured with multiple reliable servers"
 echo "Mirrors: Optimized for best performance"
 echo "=========================================="
+
+pacman-key --init
+pacman-key --populate
