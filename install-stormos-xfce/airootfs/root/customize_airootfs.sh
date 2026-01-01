02a43080400cfa165e85ac04d95395dc46190846
bfitzgit23 <dfitz@me.com>
2026-01-01 17:04:24 -0500

    fixed grub theme name in rootfs script

--- install-stormos-xfce/airootfs/root/customize_airootfs.sh
+++ install-stormos-xfce/airootfs/root/customize_airootfs.sh
@@ -86,7 +86,7 @@ plymouth-set-default-theme stormos
 mkdir -p /usr/share/themes/grub/fonts
 
 # Copy StormOS theme
-cp -a /usr/share/grub/themes/stormos-grub-theme /usr/share/themes/grub/
+cp -a /usr/share/grub/themes/stormos /usr/share/themes/grub/
 
 # Ensure proper permissions
 chown -R root:root /usr/share/themes/grub
