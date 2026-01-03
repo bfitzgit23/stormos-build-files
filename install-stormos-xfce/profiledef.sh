21ac934f322b3e0c447b0e54da4cf0a9b84ee634
ben <dfitz@me.com>
2026-01-03 00:54:25 -0500

    Update ISO version to January-Point 1

--- install-stormos-xfce/profiledef.sh
+++ install-stormos-xfce/profiledef.sh
@@ -5,7 +5,7 @@ iso_name="StormOS_Oblivion"
 iso_label="StormOS_Oblivion_Release"
 iso_publisher="StormOS"
 iso_application="StormOS Install Media"
-iso_version="December-Point 2"
+iso_version="January-Point 1"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
@@ -13,7 +13,7 @@ bootmodes=('bios.syslinux'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
